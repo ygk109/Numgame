4a20b63acc734bf88a96b7e6d909caa9a7b27740
ygk109 <ygk5192@gmail.com>
2025-02-16 13:40:51 +0900

    initial spring settings and db connection test.

--- pointgame/pointgame/src/main/java/com/ksinfo/pointgame/controller/MemberController.java
+++ pointgame/pointgame/src/main/java/com/ksinfo/pointgame/controller/MemberController.java
@@ -1,5 +1,29 @@
 package com.ksinfo.pointgame.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
 public class MemberController {
 
-}
+	    @GetMapping("/status")
+	    public String pointGamePage() {
+	        return "동작중";
+	    }
+	    
+	    @Autowired
+	    private JdbcTemplate jdbcTemplate;
+
+	    @GetMapping("/db")
+	    public List<Map<String, Object>> getTables() {
+	        String sql = "SELECT * FROM memberinfo";
+	        return jdbcTemplate.queryForList(sql);
+	    }
+	}
